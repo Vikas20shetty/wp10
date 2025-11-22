3b273db2f9ebd46ce90e38156030b8b6c142683c
Vikas M <vikas20012005@gmail.com>
2025-11-22 00:14:54 -0800

    Update 3.cookie.php

--- 3.cookie.php
+++ 3.cookie.php
@@ -1,17 +1,18 @@
- <!DOCTYPE html> 
+<!DOCTYPE html> 
 <?php 
-//LAB EXERCISE 03 
-/* Design a form to input student name and Phone number.  
-Write a PHP script to create a cookie with name as username with  
-student name as value that automatically expires after 60 minutes.*/ 
  
 $cookie_name = "bca_5"; 
 $cookie_value = "bca_student"; 
-setcookie($cookie_name, $cookie_value, time() + 1800, "/"); // 3600 is 60 minutes 
+setcookie($cookie_name, $cookie_value, time() + 1800, "/"); 
  
-if(!isset($_COOKIE[$cookie_name])) { 
+if(!isset($_COOKIE[$cookie_name]))
+{ 
     echo "Cookie named '" . $cookie_name . "' is not set!"; 
-} else { 
+} 
+
+else 
+ 
+{ 
     echo "Cookie '" . $cookie_name . "' is set!<br>"; 
     echo "Value is: " . $_COOKIE[$cookie_name]; 
 } 
