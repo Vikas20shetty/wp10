b86df721551bb15727dd7b491463168d55fb591d
Vikas M <vikas20012005@gmail.com>
2025-11-23 19:05:00 -0800

    Update 9.colors_app.js

--- 9.colors_app.js
+++ 9.colors_app.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react'; 
- 
-function App() { 
- 
-  const [color, setColor] = useState('black'); 
-  const [index, setIndex] = useState(0); 
-   
-  // Function to change the color randomly 
-  const changeColor = () => { 
-    const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'brown']; 
-    const newColor = colors[index]; 
-    setColor(newColor); 
-    setIndex((prevIndex) => (prevIndex + 1) % colors.length); 
-  }; 
-  return ( 
-    <div> 
-      <h1>Change Paragraph Color</h1> 
-      <p style={{ color: color, fontSize: '20px' }}> 
-        Click the button to change my color! </p> 
-      <button onClick={changeColor} > Change Color</button> 
-    </div> 
-  ); 
-} 
-export default App;
+import React, { useState } from 'react';
+
+export default function App() {
+  const colors = ['red','blue','green','purple','orange','pink','brown'];
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div>
+      <h1>Change Paragraph Color</h1>
+      <p style={{ color: colors[index], fontSize: 20 }}>
+        Click the button to change my color!
+      </p>
+      <button onClick={() => setIndex((i) => (i + 1) % colors.length)}>
+        Change Color
+      </button>
+    </div>
+  );
+}
