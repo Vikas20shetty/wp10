1f8d76dc1a3fce0353e346b331cfedca4afac743
Vikas M <vikas20012005@gmail.com>
2025-11-23 18:40:00 -0800

    Update 8. counter_app.js

--- 8. counter_app.js
+++ 8. counter_app.js
@@ -1,26 +1,14 @@
-import React, { useState } from 'react'; 
- 
-const Counter = () => { 
-  const [count, setCount] = useState(0); 
-  const increment = () => setCount(prevCount => prevCount + 1); 
-  const decrement = () => setCount(prevCount => prevCount - 1); 
-  const reset = () => setCount(0); 
-  return ( 
-    <div> 
-      <h2>Count: {count}</h2> 
-      <button onClick={increment}>Increment</button> 
-      <button onClick={decrement}>Decrement</button> 
-      <button onClick={reset}>Reset</button> 
-    </div> 
-  ); 
-}; 
- 
-const App = () => { 
-  return ( 
-    <div><h1>Simple Counter App</h1> 
-      <Counter /> 
-    </div> 
-  ); 
-}; 
- 
-export default App;
+import React, { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Simple Counter App</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(c => c + 1)}>+</button>
+      <button onClick={() => setCount(c => c - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
