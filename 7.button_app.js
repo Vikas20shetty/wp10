78e199b01bf2f4cd16854388f60bd663a8ecc0be
Vikas M <vikas20012005@gmail.com>
2025-11-23 18:27:59 -0800

    Update 7.button_app.js

--- 7.button_app.js
+++ 7.button_app.js
@@ -1,23 +1,24 @@
-import { useState } from "react"; 
-import ReactDOM from 'react-dom/client'; 
- 
-function MyForm() { 
-  const [name, setName] = useState(""); 
-  const handleSubmit = (event) => { 
-    event.preventDefault(); 
-    alert(`The name you entered was: ${name}`); 
-  } 
- 
-  return ( 
-    <form onSubmit={handleSubmit}> 
-       <label>Enter your name: 
-         <input  
-            type="text"  
-            value={name} 
-            onChange={(e) => setName(e.target.value)} 
-          /> 
-      </label> 
-   <input type="submit" /> 
-  </form> 
-  ) 
-} 
+import { useState } from "react";
+
+function MyForm() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(`Name: ${name}`); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <lable>Please Enter your Name:</lable><br/>
+      <input
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)} 
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default MyForm;
