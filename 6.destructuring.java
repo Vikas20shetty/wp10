adc61fa48e38ad5f1558de10b7569133b1f7a21a
Vikas M <vikas20012005@gmail.com>
2025-11-23 17:51:34 -0800

    Update 6.destructuring.java

--- 6.destructuring.java
+++ 6.destructuring.java
@@ -1,30 +1,14 @@
-//array destructuring 
-let colors = ['red', 'green', 'blue', 'violet', 'black']; 
-let [value1, value2,,, value3] = colors;  //array destructuring using [] 
-console.log('Colors after destructuring'); 
-console.log(value1, value2, value3); 
- 
-//object destructuring 
-let obj = { 
-    id: 123, 
-    sname: 'ABC', 
-    percent: 94 
-}; 
-let { id, sname, percent } = obj;   //object destructuring using {} 
-console.log('Object after destructuring'); 
+let colors = ['red', 'green', 'blue', 'violet', 'black'];
+let [c1, c2, , , c3] = colors;
+console.log('Array after destructing');
+console.log(c1, c2, c3); 
+
+
+let { id, sname, percent } = { id: 123, sname: 'ABC', percent: 94 };
+console.log('Object after destructing');
 console.log(id, sname, percent); 
- 
-//arrow function 
-//function to calculate factorial 
- 
-/* let fact=(n)=>{ 
-    let f=1; 
-    for (let i = 2; i <= n; i++) { 
-        f *= i; 
-    } 
-    return f; 
-}; 
- */ 
- 
-let fact = (n) => (n == 0 ? 1 : n * fact(n - 1)); 
-console.log(`Factorial of 5 is ${fact(4)}`);
+
+
+let fact = n => (n ? n * fact(n - 1) : 1);
+console.log('Using arrow function');
+console.log(fact(5)); 
