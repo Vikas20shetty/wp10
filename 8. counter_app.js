c8e010d908d8ee2b624d70f9e7a5c5e5769720ba
Vikas M <vikas20012005@gmail.com>
2025-11-23 18:44:09 -0800

    Update 8. counter_app.js

--- 8. counter_app.js
+++ 8. counter_app.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
-export default function App() {
+function App() 
+{
   const [count, setCount] = useState(0);
+
   return (
+
     <div>
       <h1>Simple Counter App</h1>
       <h2>Count: {count}</h2>
+
       <button onClick={() => setCount(c => c + 1)}>+</button>
       <button onClick={() => setCount(c => c - 1)}>-</button>
       <button onClick={() => setCount(0)}>Reset</button>
+
     </div>
   );
 }
+export default  App;
